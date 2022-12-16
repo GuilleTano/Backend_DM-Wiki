d541d8b843e082708c02596cb1371a481c080af6
GuilleTano <110430498+GuilleTano@users.noreply.github.com>
2022-12-16 17:23:42 -0300

    Redireccion a BD correcta.

--- src/dbConnection/connection.js
+++ src/dbConnection/connection.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 
 const password = "MVNJBjhemE80wq6c";
 const uri = `mongodb+srv://guille_atlas_03:${password}@cluster0.bqtubmo.mongodb.net/?retryWrites=true&w=majority`;
+const options = {dbName: "digipedia"};
 
-module.exports = ()=> mongoose.connect(uri);
+
+module.exports = ()=> mongoose.connect(uri, options);
 /* Esto es lo mismo que lo de arriba pero con mas codigo
 const connection = ()=> {
     mongoose.connect(uri, {userNewUrlParse: true, useUnifiedTopology: true});
@@ -13,4 +15,4 @@ module.exports = connection;
 
 
 // para el error que tira el mongoose
-//mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', true);
