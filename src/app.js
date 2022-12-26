dda74330655f4f43535e79d498c63fcfe8188f7e
GuilleTano <guillermotano@hotmail.com>
2022-12-26 13:51:49 -0300

    Eliminar codigo sobrante

--- src/app.js
+++ src/app.js
@@ -9,34 +9,9 @@ app.use(cors());
 app.use(express.json());
 
 
-
-//const fs = require("fs");
-//app.use(express.json());
-
-
 // ROUTES
 const routes = require("./routes/index.routers");
 app.use(routes);
-
-
-/*
-app.post("/LINK-DEL-POST", (req, res) =>{
-
-    const {id, name, img, attr, skills} = req.body;
-
-    let newObj ={
-        id: id,
-        name: name,
-        img: img,
-        attr: attr,
-        skills: skills
-    };
-
-    const json_newObj = JSON.stringify(newObj);
-    fs.writeFileSync("json/lista_Digimon.json", json_newObj, "utf-8");
-
-});
-*/
 
 
 app.listen(3000, ()=>{
